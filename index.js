b58334604964d6a61aa445984abe29eedd7f13d3
Ben Foxall <ben@whiteoctober.co.uk>
2014-04-01 19:45:47 +0100

    save the image

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.bodyParser());
-
-app.get('/', function(req, res){
-  res.send('COFFEE?!\n');
-});
 
 if(process.env.USERNAME && process.env.PASSWORD)
   app.post('/',express.basicAuth(process.env.USERNAME, process.env.PASSWORD))
 
 app.post('/', function(req, res){
+
   res.send('COFFEE!!!\n');
+
+  fs.readFile(req.files.file.path, function (err, data) {
+    var newPath = __dirname + "/public/recent.jpg";
+    fs.writeFile(newPath, data, function (err) {
+      if(err) console.err(err);
+      console.log("written!")
+    });
+  });
 })
+
+// serve all files in this directory
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
