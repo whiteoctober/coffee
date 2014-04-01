c487fbf2ba20c7ef3a578a3bd37bbf41aed5ec5a
Ben Foxall <ben@whiteoctober.co.uk>
2014-04-01 19:24:18 +0100

    POST / - with http basic auth

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.bodyParser());
 
 app.get('/', function(req, res){
   res.send('COFFEE?!\n');
 });
+
+if(process.env.USERNAME && process.env.PASSWORD)
+  app.post('/',express.basicAuth(process.env.USERNAME, process.env.PASSWORD))
+
+app.post('/', function(req, res){
+  res.send('COFFEE!!!\n');
+})
 
 app.listen(process.env.PORT || 3000);
